8298f06fdbd12cfea306492a4396377c68ca4320
jlf <931980377@qq.com>
2025-01-20 14:04:01 +0500

    项目启动

--- src/main/java/com/jlf/music/JMMusicApplication.java
+++ src/main/java/com/jlf/music/JMMusicApplication.java
@@ -1,7 +1,17 @@
 package com.jlf.music;
 
-public class Main {
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@MapperScan("com.jlf.music.mapper")
+@SpringBootApplication
+public class JMMusicApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(JMMusicApplication.class, args);
+        log.info("====================================项目启动成功, SpringBoot Version：[{}]====================================",
+                SpringApplication.class.getPackage().getImplementationVersion());
     }
 }
